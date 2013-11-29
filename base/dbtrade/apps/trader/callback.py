18e7fc45818e529d7dff0ce38ffdee0d993c2663
Abel Mohler <abel@wayfarerweb.com>
2013-11-29 14:57:19 -0500

    Updates to get started on callback handling for payment buttons.

---
+++ base/dbtrade/apps/trader/callback.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+from datetime import datetime
+from decimal import Decimal
+
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template import Context, RequestContext
+from django import forms
+#from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login as auth_login, logout as auth_logout
+
+from dbtrade.apps.trader.models import TickerHistory
+
+
+def access_fee(request):
+    if request.method == 'POST':
+        with open('/tmp/last_request.txt') as f:
+            f.write(str(request.body))
+    env = {}
+    return render_to_response('about.html', RequestContext(request, env))
