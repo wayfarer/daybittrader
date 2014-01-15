4c3c52fd41ddeccacd5c15ebe57ff2844e9bbe58
Abel Mohler <abel@wayfarerweb.com>
2014-01-14 19:08:41 -0500

    Undo step 2 update.

--- base/dbtrade/apps/trader/callback.py
+++ base/dbtrade/apps/trader/callback.py
@@ -42,9 +42,8 @@ def connect_coinbase_callback(request):
     if oauth_code == None:
         raise Http404
     
-    #http = httplib2.Http(ca_certs='/etc/ssl/certs/ca-certificates.crt')
-    #token = coinbase_client.step2_exchange(oauth_code, http=http)
-    token = coinbase_client.step2_exchange(oauth_code)
+    http = httplib2.Http(ca_certs='/etc/ssl/certs/ca-certificates.crt')
+    token = coinbase_client.step2_exchange(oauth_code, http=http)
     
     request.user.usersettings.coinbase_oauth_token = token
     request.user.usersettings.save()
