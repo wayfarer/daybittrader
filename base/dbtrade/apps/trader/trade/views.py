6a9048645a89966eaf360d4c29605f48e65d0e2c
Abel Mohler <abel@wayfarerweb.com>
2014-01-20 21:14:10 -0500

    First attempt at a basic buy/sell form for trading in the UI.

--- base/dbtrade/apps/trader/trade/views.py
+++ base/dbtrade/apps/trader/trade/views.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime
+
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.conf import settings
 from django.shortcuts import render_to_response
@@ -7,6 +9,7 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
 from dbtrade.utils.utils import get_user_cb_api
+from dbtrade.apps.trader.models import TradeOrder
 
 
 @login_required(login_url='/#login-form')
@@ -29,24 +32,58 @@ def login(request):
     return render_to_response(template, RequestContext(request, env))
 
 
-@login_required(login_url='/#login-form')
-def buy(request):
+DELTA_TYPE_CHOICES = (('hours', 'Hours'), ('days', 'Days'), ('weeks', 'Weeks'))
+
+class TradeForm(forms.ModelForm):
+    expires = forms.IntegerField()
+    expires_type = forms.ChoiceField(widget=forms.RadioSelect, choices=DELTA_TYPE_CHOICES)
+    
+    class Meta:
+        model = TradeOrder
+        fields = ['price_point', 'btc_amount']
+        
+    def save(self, *args, **kwargs):
+        deltakwargs = {self.cleaned_data['expires_type']: self.cleaned_data['expires']}
+        this_timedelta = timedelta(**deltakwargs)
+        self.instance.date_expire = datetime.utcnow() + this_timedelta
+        super(TradeForm, self).save(*args, **kwargs)
+
+
+def trade(request, trade_type):
     template = 'trade_order.html'
     CB_API = get_user_cb_api(request.user)
     if CB_API == None:
-        return HttpResponseRedirect('/trade/login/?ref=/trade/buy/')
+        return HttpResponseRedirect('/trade/login/?ref=/trade/%s/' % trade_type.lower())
     
-    env = {}
+    trade = None
+    if request.method == 'POST':
+        form = TradeForm(request.POST)
+    else:
+        form = TradeForm(initial={'expires': 2, 'expires_type': 'days'})
+        
+    #: TODO: probably other logic here
+        
+    if request.method == 'POST':
+        if form.is_valid():
+            form.instance.type = trade_type
+            trade = form.save()
+            return HttpResponseRedirect('/trade/%s/' % trade_type.lower())
+    
+    env = {
+           'submit_text': 'Create Purchase Order',
+           'form': form,
+           'trade': trade,
+           'trade_type': trade_type
+           }
     return render_to_response(template, RequestContext(request, env))
+
+
+@login_required(login_url='/#login-form')
+def buy(request):
+    return trade(request, 'BUY')
 
 
 @login_required(login_url='/#login-form')
 def sell(request):
-    template = 'trade_order.html'
-    CB_API = get_user_cb_api(request.user)
-    if CB_API == None:
-        return HttpResponseRedirect('/trade/login/?ref=/trade/sell/')
-    
-    env = {}
-    return render_to_response(template, RequestContext(request, env))
+    return trade(request, 'SELL')
 
