dbda7fd19242dc0d60e7f99c174256832c230e49
Abel Mohler <abel@wayfarerweb.com>
2014-01-24 12:13:07 -0500

    Basic live updating of Bitstamp price on trade order pages.

--- base/dbtrade/apps/trader/trade/views.py
+++ base/dbtrade/apps/trader/trade/views.py
@@ -106,6 +106,7 @@ def trade(request, trade_type):
            'trade_type_lower': trade_type.lower(),
            'cb_balance': CB_API.balance,
            'current_price': current_price_dict[trade_type],
+           'bitstamp_price': '%.2f' % current_ticker.bs_last,
            'trades': trades_data
            }
     return render_to_response(template, RequestContext(request, env))
--- base/dbtrade/assets/static/js/trade.js
+++ base/dbtrade/assets/static/js/trade.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
 	var pusher = new Pusher('de504dc5763aeef9ff52');
 	var trades_channel = pusher.subscribe('live_trades');
 	trades_channel.bind('trade', function(data) {
-		console.log(data);
+		//console.log(data);
+		$('.bs-last').html('$' + data.price);
 	});
-    var order_book_channel = pusher.subscribe('order_book');
+    /*var order_book_channel = pusher.subscribe('order_book');
     order_book_channel.bind('data', function(data) {
     	console.log(data);
-    });
+    });*/
 });
