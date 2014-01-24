3786588d0ea04647f81d03d9bc7b0ec9b4c81c80
Abel Mohler <abel@wayfarerweb.com>
2014-01-24 11:56:16 -0500

    Updates to test live pusher results for bitstamp.

---
+++ base/dbtrade/assets/static/js/trade.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var pusher = new Pusher('de504dc5763aeef9ff52');
+	var trades_channel = pusher.subscribe('live_trades');
+	trades_channel.bind('trade', function(data) {
+		console.log(data);
+	});
+    var order_book_channel = pusher.subscribe('order_book');
+    order_book_channel.bind('data', function(data) {
+    	console.log(data);
+    });
+});
