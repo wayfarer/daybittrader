3a7932894d69d43f43f2295c48940076cb77e344
Abel Mohler <abel@wayfarerweb.com>
2014-01-28 15:35:13 -0500

    Updated REDIS_HOST to allow external connection.

--- base/dbtrade/config/production.py
+++ base/dbtrade/config/production.py
@@ -2,4 +2,6 @@ DEFAULT_DB_USER = 'dbtrade'
 DEFAULT_DB_PASSWORD = 'orange123'
 DEFAULT_DB_HOST = '10.147.187.23'
 
+REDIS_HOST = '10.147.187.23'
+
 ALLOWED_HOSTS = ['daybittrader.com', 'www.daybittrader.com']
