40dfe28a6b77bf8ffd90469fd5c179b44939684f
Abel Mohler <abel@wayfarerweb.com>
2014-01-27 22:29:23 -0500

    Update production settings to use new external db.

--- base/dbtrade/config/production.py
+++ base/dbtrade/config/production.py
@@ -1,5 +1,5 @@
 DEFAULT_DB_USER = 'dbtrade'
 DEFAULT_DB_PASSWORD = 'orange123'
-DEFAULT_DB_HOST = ''
+DEFAULT_DB_HOST = '10.147.187.23'
 
 ALLOWED_HOSTS = ['daybittrader.com', 'www.daybittrader.com']
