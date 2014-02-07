da71b84d41b6e374a353316baae2fe61e9919667
Abel Mohler <abel@wayfarerweb.com>
2014-02-06 22:39:34 -0500

    Updated debugging output.

--- base/dbtrade/apps/trader/tasks.py
+++ base/dbtrade/apps/trader/tasks.py
@@ -187,6 +187,7 @@ def do_trades(estimated_buy_price, estimated_sell_price):
         for trade_queryset in args:
             trade_queryset.select_for_update().update(locked=True)
             for trade_order in trade_queryset:
+                print trade_order.id
                 trade.delay(trade_order.id)
     
     _trade_queryset_shared = TradeOrder.objects.filter(locked=False, active=True, date_expire__gt=now())
