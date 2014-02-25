ce004b0b0ccf776de1ffa602f04e8de49efe216c
Abel Mohler <abel@wayfarerweb.com>
2014-02-25 07:42:32 -0500

    Make all mtgox fields nullable.

--- base/dbtrade/apps/trader/models.py
+++ base/dbtrade/apps/trader/models.py
@@ -27,25 +27,25 @@ class TickerHistory(LogModel):
     volume = models.DecimalField(max_digits=18, decimal_places=8, null=True)
     
     weighted_average_value  = models.DecimalField(max_digits=12, decimal_places=5, null=True)
-    weighted_average_value_int = models.IntegerField()
+    weighted_average_value_int = models.IntegerField(null=True)
     
     average_value = models.DecimalField(max_digits=12, decimal_places=5, null=True)
-    average_value_int = models.IntegerField()
+    average_value_int = models.IntegerField(null=True)
     
     last_value = models.DecimalField(max_digits=12, decimal_places=5, null=True)
-    last_value_int = models.IntegerField()
+    last_value_int = models.IntegerField(null=True)
     
     high_value = models.DecimalField(max_digits=12, decimal_places=5, null=True)
-    high_value_int = models.IntegerField()
+    high_value_int = models.IntegerField(null=True)
     
     low_value = models.DecimalField(max_digits=12, decimal_places=5, null=True)
-    low_value_int = models.IntegerField()
+    low_value_int = models.IntegerField(null=True)
     
     sell_value = models.DecimalField(max_digits=12, decimal_places=5, null=True)
-    sell_value_int = models.IntegerField()
+    sell_value_int = models.IntegerField(null=True)
     
     buy_value = models.DecimalField(max_digits=12, decimal_places=5, null=True)
-    buy_value_int = models.IntegerField()
+    buy_value_int = models.IntegerField(null=True)
     
     cb_buy_value = models.DecimalField(max_digits=12, decimal_places=5, null=True)
     cb_buy_value_50 = models.DecimalField(max_digits=12, decimal_places=5, null=True)
@@ -59,7 +59,7 @@ class TickerHistory(LogModel):
     bs_low = models.DecimalField(max_digits=12, decimal_places=2, null=True)
     bs_volume = models.DecimalField(max_digits=18, decimal_places=8, null=True)
     
-    mtgox_timestamp = models.CharField(max_length=100)
+    mtgox_timestamp = models.CharField(max_length=100, null=True)
     
     
 class IntervalHistory(models.Model):
