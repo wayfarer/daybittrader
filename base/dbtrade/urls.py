694b2613c239d01000ff55c2db3f4c7b34e868cc
Abel Mohler <abel@wayfarerweb.com>
2014-03-07 08:16:28 -0500

    Add robots.txt

--- base/dbtrade/urls.py
+++ base/dbtrade/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -21,4 +22,6 @@ urlpatterns = patterns('',
     
     url(r'^trade/', include('dbtrade.apps.trader.trade.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 )
